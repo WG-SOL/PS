6085c364b775a7b1ea4839da0595c3a1d5f6be2f
WG-SOL <32182825@dankook.ac.kr>
2025-05-22 12:33:43 +0900

    Title: 비교 연산, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250522/비교 연산/comparison-operator.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A,B;
+    cin >> A >> B;
+
+    if(A>=B) cout << 1 << "\n";
+    if(A>=B) cout << 1 << "\n";
+    if(A>=B) cout << 1 << "\n";
+
+
+    return 0;
+}
