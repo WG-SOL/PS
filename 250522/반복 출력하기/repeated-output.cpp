fa283bebb9d2a997f9fc2dc0839e0cd2e1152cd3
WG-SOL <32182825@dankook.ac.kr>
2025-05-22 15:44:43 +0900

    Title: 반복 출력하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250522/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+void print(int num){
+    cout << "12345^&*()_\n";
+}
+
+
+
+
+
+int main() {
+    cin >> N;
+
+    print(N);
+
+    return 0;
+}
