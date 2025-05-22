092a8d6a8bd8efc1adaa577c496aa788c5c1638e
WG-SOL <32182825@dankook.ac.kr>
2025-05-22 11:31:10 +0900

    Title: 달에서 무게 구하기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250522/달에서 무게 구하기/weight-on-the-moon.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int weight = 13;
+    double gravity = 0.165;
+
+    cout << weight << " * " << gravity << " = " << weight*gravity;
+    return 0;
+}
