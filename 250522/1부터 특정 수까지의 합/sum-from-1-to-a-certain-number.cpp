8f2a2d6180d436e5f028f562deda8594bf4c8f6c
WG-SOL <32182825@dankook.ac.kr>
2025-05-22 16:42:04 +0900

    Title: 1부터 특정 수까지의 합, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250522/1부터 특정 수까지의 합/sum-from-1-to-a-certain-number.cpp
+++ 250522/1부터 특정 수까지의 합/sum-from-1-to-a-certain-number.cpp
@@ -7,10 +7,9 @@ int N;
 int sum(int n){
     int temp=0;
     
-    for(int i=0; i<n/2; i++){
-        temp += (i + (n-i));
+    for(int i=0; i<n; i++){
+        temp += i;
     }
-    temp += n/2;
 
     return temp/10;
 }
