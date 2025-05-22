d89487149a7c3ff1b706bd9cd40fc544bf2f0789
WG-SOL <32182825@dankook.ac.kr>
2025-05-22 12:34:55 +0900

    Title: 비교 연산, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250522/비교 연산/comparison-operator.cpp
+++ 250522/비교 연산/comparison-operator.cpp
@@ -5,9 +5,12 @@ int main() {
     int A,B;
     cin >> A >> B;
 
-    if(A>=B) cout << 1 << "\n";
-    if(A>=B) cout << 1 << "\n";
-    if(A>=B) cout << 1 << "\n";
+    if(A>=B) cout << 1 << "\n"; else cout << 0 << "\n";
+    if(A>B) cout << 1 << "\n"; else cout << 0 << "\n";
+    if(A<=B) cout << 1 << "\n"; else cout << 0 << "\n";
+    if(A<B) cout << 1 << "\n"; else cout << 0 << "\n";
+    if(A==B) cout << 1 << "\n"; else cout << 0 << "\n";
+    if(A!=B) cout << 1 << "\n"; else cout << 0 << "\n";
 
 
     return 0;
