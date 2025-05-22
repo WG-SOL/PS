b2577c04536f4c3a0ab50be31f3d7d7918336c30
WG-SOL <32182825@dankook.ac.kr>
2025-05-22 15:13:13 +0900

    Title: 별 찍는 것을 5번 반복하기, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250522/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void asterisk(){
+    cout << "**********\n"; 
+}
+
+
+int main() {
+    
+    for(int i=0; i<5; i++){
+        asterisk();
+    }
+
+    return 0;
+}
