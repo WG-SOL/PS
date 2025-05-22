1deb8a8ba28733bb69c4f4e5c8450a178a0aa404
WG-SOL <32182825@dankook.ac.kr>
2025-05-22 12:24:03 +0900

    Title: 합과 평균의 차, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250522/합과 평균의 차/sub-of-average-and-sum.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b,c;
+    con >> a >> b >> c;
+
+    int sum=a+b+c;
+    int avg = (a + b + c)/3;
+
+    cout << sum << "\n" << avg << "\n" << sum-avg;
+
+    return 0;
+}
