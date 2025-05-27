0210178f61823462f6cd2e5ae609d6bb7dbd27c7
WG-SOL <32182825@dankook.ac.kr>
2025-05-27 22:50:01 +0900

    Title: 금 채굴하기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250527/금 채굴하기/gold-mining.cpp
+++ 250527/금 채굴하기/gold-mining.cpp
@@ -1,30 +1,30 @@
 #include <iostream>
 #include <algorithm>
+#include <cmath>
 using namespace std;
 
 int n, m;
 int grid[20][20];
 
-int GetArea(int k) {
+int cost(int k) {
     return k * k + (k + 1) * (k + 1);
 }
 
-// 마름모 범위로 금의 개수를 셈
-int GetNumOfGold(int row, int col, int k) {
+int gold_count(int x, int y, int k) {
     int num_of_gold = 0;
     for (int d = 0; d <= k; d++) {
         for (int dx = -d; dx <= d; dx++) {
             int dy = d - abs(dx);
 
-            // (row+dx, col+dy)
-            int nx1 = row + dx;
-            int ny1 = col + dy;
+            
+            int nx1 = x + dx; // 위쪽, 오른쪽
+            int ny1 = y + dy;
             if (nx1 >= 0 && nx1 < n && ny1 >= 0 && ny1 < n)
                 num_of_gold += grid[nx1][ny1];
 
-            // (row+dx, col-dy) (dy != 0일 때만 중복 방지)
-            if (dy != 0) {
-                int ny2 = col - dy;
+            
+            if (dy != 0) { // 왼쪽, 오른쪽
+                int ny2 = y - dy;
                 if (nx1 >= 0 && nx1 < n && ny2 >= 0 && ny2 < n)
                     num_of_gold += grid[nx1][ny2];
             }
@@ -40,11 +40,11 @@ int main() {
             cin >> grid[i][j];
 
     int max_gold = 0;
-    for (int row = 0; row < n; row++) {
-        for (int col = 0; col < n; col++) {
+    for (int x = 0; x < n; x++) {
+        for (int y = 0; y < n; y++) {
             for (int k = 0; k <= 2 * (n - 1); k++) {
-                int num_of_gold = GetNumOfGold(row, col, k);
-                if (num_of_gold * m >= GetArea(k))
+                int num_of_gold = gold_count(x, y, k);
+                if (num_of_gold * m >= cost(k))
                     max_gold = max(max_gold, num_of_gold);
             }
         }
