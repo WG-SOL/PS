4cc33e1ec4baf97840d4ab13c687d6a78e5b9de3
WG-SOL <32182825@dankook.ac.kr>
2025-05-27 11:51:21 +0900

    Title: 행복한 수열의 개수, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250527/행복한 수열의 개수/number-of-happy-sequence.cpp
+++ 250527/행복한 수열의 개수/number-of-happy-sequence.cpp
@@ -8,38 +8,42 @@ int grid[100][100];
 int counter;
 
 bool row_find(int row, int num){
-    int temp=0;
+    int temp=1;
     bool flag = false;
 
         for(int j=0; j<n-1; j++){
             if(grid[row][j] == grid[row][j+1]){
                 temp++; 
-                flag=true;
-                if(temp == num) return flag;
+                if(temp == num) {
+                    flag = true;
+                    return flag;
+                }
             }
             else{
-                flag = false;
+                temp=1;
             }
         }
-    
+    flag = false;
     return flag;
 }
 
 bool col_find(int col, int num){
-    int temp=0;
+    int temp=1;
     bool flag = false;
-    
+
         for(int i=0; i<n-1; i++){
             if(grid[i][col] == grid[i+1][col]){
-                temp++; 
-                flag=true;
-                if(temp == num) return flag;
+                temp++;
+                if(temp == num) {
+                    flag = true;
+                    return flag;
+                }
             }
             else{
-                flag = false;
+                temp=1;
             }
         }
-
+    flag = false;
     return flag;
 }
 
@@ -62,10 +66,6 @@ int main() {
         }
         cout << counter;
     }
-
-    
-    
-    // Please write your code here.
 
     return 0;
 }
