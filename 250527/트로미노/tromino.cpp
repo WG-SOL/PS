a9e232539dce80a7eab0d4aa66be3a222044e127
WG-SOL <32182825@dankook.ac.kr>
2025-05-27 20:05:23 +0900

    Title: 트로미노, Time: 11ms, Memory: 0MB, Status: Passed - Codetree

--- 250527/트로미노/tromino.cpp
+++ 250527/트로미노/tromino.cpp
@@ -4,86 +4,51 @@ using namespace std;
 
 int n, m;
 int grid[200][200];
-int max_value;
+int max_value = 0;
 
-void neon(int x, int y){
-    int sumation=0;
+void neon(int x, int y) {
+    // 중심 기준 3칸의 L자(ㄱ, ㄴ, 반대 ㄱ, 반대 ㄴ)
+    // ㄱ자: (x, y), (x, y-1), (x+1, y)
+    if (y - 1 >= 0 && x + 1 < n)
+        max_value = max(max_value, grid[x][y] + grid[x][y - 1] + grid[x + 1][y]);
 
-    if(x>=0 && y>=0 && x<n && y<m){  // 기본 x,y
+    // ㄴ자: (x, y), (x-1, y), (x, y+1)
+    if (x - 1 >= 0 && y + 1 < m)
+        max_value = max(max_value, grid[x][y] + grid[x - 1][y] + grid[x][y + 1]);
 
-        if(x>=0&&(y-1)>=0 && x<n && (y-1)<m){ // ㄱ
-            if((x+1)>=0 && y>=0 && (x+1)<n && y<m){
-                
-                sumation = max(sumation, grid[x][y] + grid[x][y-1] + grid[x+1][y]);
-            }
-        }                
+    // 반대 ㄱ: (x, y), (x, y+1), (x+1, y)
+    if (y + 1 < m && x + 1 < n)
+        max_value = max(max_value, grid[x][y] + grid[x][y + 1] + grid[x + 1][y]);
 
-        if((x-1)>=0 && y>=0 && (x-1)<n && y<m){ // ㄴ
-            if(x>=0 && (y+1)>=0 && x<n && (y+1)<m){
-                
-                sumation = max(sumation, grid[x][y]+grid[x-1][y]+grid[x][y+1]);
-            }
-        }
-
-        if(x>=0 && (y+1)>=0 && x<n && (y+1)<m){ // 반대 ㄱ
-            if((x+1)>=0 && y>=0 && (x+1)<n && y<m){
-                
-                    sumation = max(sumation, grid[x][y]+grid[x][y+1]+grid[x+1][y]);
-                
-            }
-        }               
-                        
-        if((x-1)>=0 && y>=0 && (x-1)<n && y<m){ //반대 ㄴ
-            if(x>=0 && (y-1)>=0 && x<n && (y-1)<m){
-                
-                    sumation = max(sumation, grid[x][y]+grid[x-1][y]+grid[x][y-1]);
-                
-            }
-        }                
-    }
-    max_value = max(sumation,max_value);
+    // 반대 ㄴ: (x, y), (x-1, y), (x, y-1)
+    if (x - 1 >= 0 && y - 1 >= 0)
+        max_value = max(max_value, grid[x][y] + grid[x - 1][y] + grid[x][y - 1]);
 }
 
-void space_bar(int x, int y){
-    int sumation=0;
-    if(x>=0 && y>=0 && x<n && y<m){ // 기본 x,y
+void space_bar(int x, int y) {
+    // 중심 기준 3칸의 일자형 (ㅡ, ㅣ)
+    // 가로 (ㅡ): (x, y-1), (x, y), (x, y+1)
+    if (y - 1 >= 0 && y + 1 < m)
+        max_value = max(max_value, grid[x][y - 1] + grid[x][y] + grid[x][y + 1]);
 
-        if(x>=0 && (y-1)>=0 && x<n && (y-1)<m){ //가로 스페이스
-            if(x>=0 && (y+1)>=0 && x<n && (y+1)<m){
-                sumation = max(sumation, grid[x][y]+grid[x][y-1]+grid[x][y+1]);
-            }
-        }
-
-        if((x-1)>=0 && y>=0 && (x-1)<n && y<m){ //세로 스페이스
-            if((x+1)>=0 && y>=0 && (x+1)<n && y<m){
-                sumation = max(sumation, grid[x][y]+grid[x-1][y]+grid[x+1][y]);
-            }
-        }
-     }
-    max_value = max(sumation,max_value);
+    // 세로 (ㅣ): (x-1, y), (x, y), (x+1, y)
+    if (x - 1 >= 0 && x + 1 < n)
+        max_value = max(max_value, grid[x - 1][y] + grid[x][y] + grid[x + 1][y]);
 }
-
-
 
 int main() {
     cin >> n >> m;
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
             cin >> grid[i][j];
-        }
-    }
 
-    // Please write your code here.
-
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
         for (int j = 0; j < m; j++) {
-            neon(i,j);
-            space_bar(i,j);
+            neon(i, j);
+            space_bar(i, j);
         }
-    }
 
     cout << max_value;
-
     return 0;
 }
