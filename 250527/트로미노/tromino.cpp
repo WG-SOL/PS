9e97e0706bcfa7d98dac30f7dd964b245df52ade
WG-SOL <32182825@dankook.ac.kr>
2025-05-27 19:50:33 +0900

    Title: 트로미노, Time: 10ms, Memory: 0MB, Status: Passed - Codetree

--- 250527/트로미노/tromino.cpp
+++ 250527/트로미노/tromino.cpp
@@ -9,35 +9,35 @@ int max_value;
 void neon(int x, int y){
     int sumation=0;
 
-    if(x>0 && y>0 && x<n && y<m){  // 기본 x,y
+    if(x>=0 && y>=0 && x<n && y<m){  // 기본 x,y
 
-        if(x>0&&(y-1)>0 && x<n && (y-1)<m){ // ㄱ
-            if((x+1)>0 && y>0 && (x+1)<n && y<m){
-                if(x != (n-2) && y != 1)
+        if(x>=0&&(y-1)>=0 && x<n && (y-1)<m){ // ㄱ
+            if((x+1)>=0 && y>=0 && (x+1)<n && y<m){
+                
                 sumation = max(sumation, grid[x][y] + grid[x][y-1] + grid[x+1][y]);
             }
         }                
 
-        if((x-1)>0 && y>0 && (x-1)<n && y<m){ // ㄴ
-            if(x>0 && (y+1)>0 && x<n && (y+1)<m){
-                if(x != 1 && y!=(m-2))
+        if((x-1)>=0 && y>=0 && (x-1)<n && y<m){ // ㄴ
+            if(x>=0 && (y+1)>=0 && x<n && (y+1)<m){
+                
                 sumation = max(sumation, grid[x][y]+grid[x-1][y]+grid[x][y+1]);
             }
         }
 
-        if(x>0 && (y+1)>0 && x<n && (y+1)<m){ // 반대 ㄱ
-            if((x+1)>0 && y>0 && (x+1)<n && y<m){
-                if(x != (n-2) && y!= (m-2)){
+        if(x>=0 && (y+1)>=0 && x<n && (y+1)<m){ // 반대 ㄱ
+            if((x+1)>=0 && y>=0 && (x+1)<n && y<m){
+                
                     sumation = max(sumation, grid[x][y]+grid[x][y+1]+grid[x+1][y]);
-                }
+                
             }
         }               
                         
-        if((x-1)>0 && y>0 && (x-1)<n && y<m){ //반대 ㄴ
-            if(x>0 && (y-1)>0 && x<n && (y-1)<m){
-                if(x != 1 && y!= 1){
+        if((x-1)>=0 && y>=0 && (x-1)<n && y<m){ //반대 ㄴ
+            if(x>=0 && (y-1)>=0 && x<n && (y-1)<m){
+                
                     sumation = max(sumation, grid[x][y]+grid[x-1][y]+grid[x][y-1]);
-                }
+                
             }
         }                
     }
@@ -46,16 +46,16 @@ void neon(int x, int y){
 
 void space_bar(int x, int y){
     int sumation=0;
-    if(x>0 && y>0 && x<n && y<m){ // 기본 x,y
+    if(x>=0 && y>=0 && x<n && y<m){ // 기본 x,y
 
-        if(x>0 && (y-1)>0 && x<n && (y-1)<m){ //가로 스페이스
-            if(x>0 && (y+1)>0 && x<n && (y+1)<m){
+        if(x>=0 && (y-1)>=0 && x<n && (y-1)<m){ //가로 스페이스
+            if(x>=0 && (y+1)>=0 && x<n && (y+1)<m){
                 sumation = max(sumation, grid[x][y]+grid[x][y-1]+grid[x][y+1]);
             }
         }
 
-        if((x-1)>0 && y>0 && (x-1)<n && y<m){ //세로 스페이스
-            if((x+1)>0 && y>0 && (x+1)<n && y<m){
+        if((x-1)>=0 && y>=0 && (x-1)<n && y<m){ //세로 스페이스
+            if((x+1)>=0 && y>=0 && (x+1)<n && y<m){
                 sumation = max(sumation, grid[x][y]+grid[x-1][y]+grid[x+1][y]);
             }
         }
