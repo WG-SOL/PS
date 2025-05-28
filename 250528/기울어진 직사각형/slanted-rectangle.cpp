686cf926c4bbe5adbac3b8fa9e647637d5a2a441
WG-SOL <32182825@dankook.ac.kr>
2025-05-28 10:13:10 +0900

    Title: 기울어진 직사각형, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250528/기울어진 직사각형/slanted-rectangle.cpp
+++ 250528/기울어진 직사각형/slanted-rectangle.cpp
@@ -5,26 +5,29 @@ using namespace std;
 int n;
 int grid[20][20];
 int max_value;
+int check;
 
 void find(int x, int y)
 {
     int value=0;
-
+    
     while(x>=0 && y>=0 && x<n && y<n){
         value += grid[x][y];
 
         if((x-1)>=0 && (y-1)>=0){
             value += grid[x-1][y-1];
+            check++;
         }
         else{
-            value -= grid[x][y];
+            value -= grid[x][y];    
         }
-
+        
         x -= 1;
         y += 1;
-        if(!((x-1)>=0 && (y-1)>=0)) break;
+        
     }
 
+    if(check<2) value=0;
     max_value = max(value,max_value);
 
 }
@@ -44,6 +47,7 @@ int main() {
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             find(i,j);
+            check=0;
         }
     }
 
