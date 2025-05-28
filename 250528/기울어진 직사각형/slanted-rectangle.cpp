83688d0c55a5110dcf34ec22b629c14e6d0ce58a
WG-SOL <32182825@dankook.ac.kr>
2025-05-28 10:16:38 +0900

    Title: 기울어진 직사각형, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250528/기울어진 직사각형/slanted-rectangle.cpp
+++ 250528/기울어진 직사각형/slanted-rectangle.cpp
@@ -1,57 +1,68 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
 int n;
 int grid[20][20];
-int max_value;
-int check;
+int max_value = 0;
 
-void find(int x, int y)
-{
-    int value=0;
-    
-    while(x>=0 && y>=0 && x<n && y<n){
-        value += grid[x][y];
+void find(int x, int y, int a, int b) {
+    int sum = 0;
+    int cx = x;
+    int cy = y;
 
-        if((x-1)>=0 && (y-1)>=0){
-            value += grid[x-1][y-1];
-            check++;
-        }
-        else{
-            value -= grid[x][y];    
-        }
-        
-        x -= 1;
-        y += 1;
-        
+    // 1. ↘ 방향 a칸
+    for (int i = 0; i < a; i++) {
+        cx += 1;
+        cy += 1;
+        if (cx < 0 || cy < 0 || cx >= n || cy >= n) return;
+        sum += grid[cx][cy];
     }
 
-    if(check<2) value=0;
-    max_value = max(value,max_value);
+    // 2. ↙ 방향 b칸
+    for (int i = 0; i < b; i++) {
+        cx += 1;
+        cy -= 1;
+        if (cx < 0 || cy < 0 || cx >= n || cy >= n) return;
+        sum += grid[cx][cy];
+    }
 
+    // 3. ↖ 방향 a칸
+    for (int i = 0; i < a; i++) {
+        cx -= 1;
+        cy -= 1;
+        if (cx < 0 || cy < 0 || cx >= n || cy >= n) return;
+        sum += grid[cx][cy];
+    }
+
+    // 4. ↗ 방향 b칸
+    for (int i = 0; i < b; i++) {
+        cx -= 1;
+        cy += 1;
+        if (cx < 0 || cy < 0 || cx >= n || cy >= n) return;
+        sum += grid[cx][cy];
+    }
+
+    max_value = max(max_value, sum);
 }
-
-
 
 int main() {
     cin >> n;
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
+    for (int i = 0; i < n; i++) 
+        for (int j = 0; j < n; j++) 
             cin >> grid[i][j];
-        }
-    }
 
-    // Please write your code here.
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            check=0;
-            find(i,j);
+            for (int a = 1; a < n; a++) {
+                for (int b = 1; b < n; b++) {
+                    find(i, j, a, b);
+                }
+            }
         }
     }
 
     cout << max_value;
-
     return 0;
 }
