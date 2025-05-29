ba6fdf5293cbc4696b7de92aa6e825d40b53a6c3
WG-SOL <32182825@dankook.ac.kr>
2025-05-29 21:10:27 +0900

    Title: 최단 Run Length 인코딩, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250529/최단 Run Length 인코딩/shortest-run-length-encoding.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <climits>
+
+using namespace std;
+
+int min_value=INT_MAX;
+
+int find_RLE(string s) {
+    
+    string result;
+    int count = 1;
+
+    for (int i = 1; i <= s.length(); i++) {
+        if (i < s.length() && s[i] == s[i - 1]) {
+            count++;
+        } else {
+            result.push_back(s[i - 1]);
+            result += to_string(count);
+            count = 1;
+        }
+    }
+
+    return (int)result.size();
+}
+
+int main() {
+    string A;
+    cin >> A;
+    
+    for(int i=0; i<A.size(); i++){
+        rotate(A.begin(), A.end()-1, A.end());
+        min_value = min(min_value,find_RLE(A));
+    }
+
+    cout << min_value;
+
+    return 0;
+}
