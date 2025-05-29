ed1384da0df5dde64ef07612f2e01fc3b6c83ccd
WG-SOL <32182825@dankook.ac.kr>
2025-05-29 22:17:58 +0900

    Title: 1차원 젠가, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250529/1차원 젠가/jenga-1d.cpp
+++ 250529/1차원 젠가/jenga-1d.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int n;
-int blocks[101];
+vector<int> blocks(101);
 int s1, e1;
 int s2, e2;
-int temp[101];
 
 int main() {
     cin >> n;
@@ -16,33 +16,38 @@ int main() {
     cin >> s1 >> e1;
     cin >> s2 >> e2;
 
-    int idx = 0;
-
     // 첫 번째 제거
-    for (int i = 1; i <= n; i++) {
-        if (i < s1 || i > e1) {
-            temp[++idx] = blocks[i];
-        }
+    for (int i = s1; i <= e1; i++) {
+        blocks[i] = 0;
     }
 
-    // temp -> blocks로 복사
-    for (int i = 1; i <= idx; i++) {
-        blocks[i] = temp[i];
+    // 첫 번째 제거 후 필터링
+    vector<int> temp;
+    for (int i = 1; i <= n; i++) {
+        if (blocks[i] != 0) temp.push_back(blocks[i]);
     }
-    n = idx;
+
+    // temp -> blocks (1-indexed 유지 위해 앞에 0 삽입)
+    blocks = {0}; // 0번은 사용 안함
+    for (int val : temp) {
+        blocks.push_back(val);
+    }
 
     // 두 번째 제거
-    idx = 0;
-    for (int i = 1; i <= n; i++) {
-        if (i < s2 || i > e2) {
-            temp[++idx] = blocks[i];
-        }
+    for (int i = s2; i <= e2 && i < blocks.size(); i++) {
+        blocks[i] = 0;
+    }
+
+    // 다시 필터링
+    temp.clear();
+    for (int i = 1; i < blocks.size(); i++) {
+        if (blocks[i] != 0) temp.push_back(blocks[i]);
     }
 
     // 출력
-    cout << idx << "\n";
-    for (int i = 1; i <= idx; i++) {
-        cout << temp[i] << "\n";
+    cout << temp.size() << "\n";
+    for (int val : temp) {
+        cout << val << "\n";
     }
 
     return 0;
