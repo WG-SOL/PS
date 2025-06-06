886561e0d5d589796a974aeb875540ef7146c184
WG-SOL <32182825@dankook.ac.kr>
2025-06-06 16:50:54 +0900

    Title: 수 등장 횟수, Time: 1070ms, Memory: 5MB, Status: Time Limit Exceed - Codetree

---
+++ 250606/수 등장 횟수/number-frequency.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+const int MAX_N = 100000;
+
+int n, m;
+int arr[MAX_N];
+int nums[MAX_N];
+
+int main() {
+    cin >> n >> m; //원소수, query수 (5,2)
+
+    unordered_map<int,int> my_m;
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i]; // (1,1,3,2,3) : 원소
+        my_m[i] = arr[i];
+    }
+
+    for (int i = 0; i < m; i++) {
+        cin >> nums[i]; // query, 없다면 0
+        int counter=0;
+        for(int j=0; j<n; j++){
+            if(my_m[j] == nums[i]) counter++;
+        }
+        cout << counter << " ";
+        counter=0;
+    }
+
+
+    //out : 몇개 존재하는가
+    
+
+    // Please write your code here.
+
+    return 0;
+}
