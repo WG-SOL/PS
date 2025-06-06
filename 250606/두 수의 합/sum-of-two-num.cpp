db28cf388d3c00ade25d4178dd3931f2b4ce0478
WG-SOL <32182825@dankook.ac.kr>
2025-06-06 18:27:38 +0900

    Title: 두 수의 합, Time: 39ms, Memory: 0MB, Status: Passed - Codetree

--- 250606/두 수의 합/sum-of-two-num.cpp
+++ 250606/두 수의 합/sum-of-two-num.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <unordered_map>
+#include <algorithm>
 using namespace std;
 
 int n, k;
@@ -8,29 +8,43 @@ int arr[100000];
 int main() {
     cin >> n >> k;
 
-    unordered_map<int, int> freq;
+    for (int i = 0; i < n; i++) cin >> arr[i];
+    sort(arr, arr + n);
 
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
-        freq[arr[i]]++;
-    }
-
+    int left = 0, right = n - 1;
     int result = 0;
 
-    for (pair<int,int> temp : freq) {
-        int y = k - temp.first;
-        if (freq.count(y)) {
-            if (temp.first == y) {
-                
-                result += temp.second * (temp.second - 1) / 2;
-            } else if (temp.first < y) {
-                
-                result += temp.second * freq[y];
+    while (left < right) {
+        int sum = arr[left] + arr[right];
+
+        if (sum == k) {
+            int l_cnt = 1, r_cnt = 1;
+            
+            while (left + 1 < right && arr[left] == arr[left + 1]) {
+                l_cnt++; 
+                left++;
             }
+            while (right - 1 > left && arr[right] == arr[right - 1]) {
+                r_cnt++; 
+                right--;
+            }
+
+            if (arr[left] == arr[right]) {
+                int count = l_cnt + r_cnt;
+                result += count * (count - 1) / 2;
+                break;
+            }
+
+            result += l_cnt * r_cnt;
+            left++; right--;
+        }
+        else if (sum < k) {
+            left++;
+        }
+        else {
+            right--;
         }
     }
 
     cout << result << "\n";
-
-    return 0;
 }
