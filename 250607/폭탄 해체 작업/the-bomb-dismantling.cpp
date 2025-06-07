139c7ceb647632353778ada1cb5e5b814d13ff1e
WG-SOL <32182825@dankook.ac.kr>
2025-06-07 22:35:52 +0900

    Title: 폭탄 해체 작업, Time: 14ms, Memory: 0MB, Status: Passed - Codetree

--- 250607/폭탄 해체 작업/the-bomb-dismantling.cpp
+++ 250607/폭탄 해체 작업/the-bomb-dismantling.cpp
@@ -4,33 +4,27 @@
 using namespace std;
 
 int N;
-int used[10001]; // 시간 슬롯 사용 여부
-vector<pair<int, int>> v;
+vector<pair<int, int>> bombs; 
+bool used[10001]; 
 
 int main() {
     cin >> N;
     for (int i = 0; i < N; i++) {
-        int score, time;
-        cin >> score >> time;
-        v.push_back({score, time});
+        int s, t;
+        cin >> s >> t;
+        bombs.push_back({s, t});
     }
 
-    // 당신이 쓴 정렬 방식 유지: 제한시간 오름차순, 같으면 점수 높은 순
-    sort(v.begin(), v.end(), [](auto a, auto b) {
-        if (a.second == b.second) return a.first > b.first;
-        return a.second < b.second;
+    sort(bombs.begin(), bombs.end(), [](auto a, auto b) {
+        return a.first > b.first;
     });
 
     int answer = 0;
-    for (auto& bomb : v) {
-        int score = bomb.first;
-        int time = bomb.second;
-
-        // 가능한 가장 늦은 시간에 해체되도록 시도
-        for (int t = time; t >= 1; t--) {
+    for (auto temp : bombs) {
+        for (int t = temp.second; t >= 1; t--) { //time
             if (!used[t]) {
-                used[t] = 1;
-                answer += score;
+                used[t] = true;
+                answer += temp.first; // score
                 break;
             }
         }
