d04560f613719f1a0762da840dd69fbf7f373dba
WG-SOL <32182825@dankook.ac.kr>
2025-06-07 13:52:02 +0900

    Title: 회의실 준비 구현, Time: 47ms, Memory: 1MB, Status: Passed - Codetree

---
+++ 250607/회의실 준비 구현/implement-scheduling-meeting-room.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n;
+int s[100000];
+int e[100000];
+vector<pair<int,int>> table;
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> s[i] >> e[i];
+        table.push_back({e[i],s[i]});
+    }
+
+    int counter=0;
+    int end=-1;
+    sort(table.begin(),table.end());
+
+    for(int i=0; i<n; i++){
+        if(table[i].second >= end){ //겹치는대로 
+            end = table[i].first;
+            counter++;
+        }
+    }
+
+    cout << counter;
+
+    // Please write your code here.
+
+    return 0;
+}
