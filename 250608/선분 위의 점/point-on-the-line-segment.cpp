575cb9184ba9a5edd29fc3a81541023ba4d8bf01
WG-SOL <32182825@dankook.ac.kr>
2025-06-08 19:26:58 +0900

    Title: 선분 위의 점, Time: 20ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250608/선분 위의 점/point-on-the-line-segment.cpp
+++ 250608/선분 위의 점/point-on-the-line-segment.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
 
 using namespace std;
@@ -9,39 +8,20 @@ int points[100000];
 int start[100000], end_p[100000];
 
 int main() {
-    cin >> n >> m; //점 선분수  
+    cin >> n >> m;
 
     for (int i = 0; i < n; i++) {
-        cin >> points[i]; // 10 30 50
+        cin >> points[i];
     }
 
     for (int i = 0; i < m; i++) {
         cin >> start[i] >> end_p[i];
-        int counter=0;
-        //(1,5) -> 0
-        //(5,21) => 1 (10)
-        //(22,59) -> 2(30,50)
-        //(210,293) -> 0
-        long long diff = end_p[i] - start[i];
-        vector<int> v(diff+1,0);
-        for(auto it= v.begin(); it != v.end(); it++){
-            *it = start[i]++;
-        }
-        
-        for(int i=0; i<n; i++){
-            auto it_s = lower_bound(v.begin(),v.end(),points[i]);
-            auto it_e = upper_bound(v.begin(),v.end(),points[i]);
-            if(it_s < it_e && *it_s == points[i]){
-                counter++;
-            }
-        }
 
-        cout << counter << "\n";
+        auto lower = lower_bound(points, points + n, start[i]);
+        auto upper = upper_bound(points, points + n, end_p[i]);
+        int count = upper - lower;
+        cout << count << "\n";
     }
-
-
-    //out : 각 선분 위 몇개의 점이 존재하는가
-    // Please write your code here.
 
     return 0;
 }
