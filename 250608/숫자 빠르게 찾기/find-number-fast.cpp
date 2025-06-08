c305652340dd618c72d868795672ad7c3d2e0c79
WG-SOL <32182825@dankook.ac.kr>
2025-06-08 18:55:41 +0900

    Title: 숫자 빠르게 찾기, Time: 179ms, Memory: 1MB, Status: Passed - Codetree

--- 250608/숫자 빠르게 찾기/find-number-fast.cpp
+++ 250608/숫자 빠르게 찾기/find-number-fast.cpp
@@ -13,17 +13,36 @@ int main() {
         cin >> arr[i];
     }
 
+    bool find = true;
+
     for (int i = 0; i < m; i++) {
         int x;
         cin >> x;
-        auto it = lower_bound(arr, arr + n, x);
-        if (it != arr + n && *it == x) {
-             cout << (it - arr + 1) << "\n";  
-        } 
-        else {
-            cout << -1 << "\n";
+        int start = 0, end=n-1;
+        while(start<=end){
+            int mid_idx = (start+end)/2;
+            int mid_value = arr[mid_idx];
+
+            if(mid_value == x){
+                cout << (mid_idx+1) << "\n";
+                find = true;
+                break;
+            }
+            else if(mid_value >x){
+                end = mid_idx-1;
+            }
+            else{
+                start = mid_idx+1;
+            }
+            find = false;   
         }
+        if(!find){
+                cout << -1 << "\n";
+            }
+
     }
+
+    
 
     // Please write your code here.
 
