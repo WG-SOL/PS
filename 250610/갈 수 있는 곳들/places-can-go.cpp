994c69aec8de8bb5644fc76ed4a126a775f30a54
WG-SOL <32182825@dankook.ac.kr>
2025-06-10 23:19:07 +0900

    Title: 갈 수 있는 곳들, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250610/갈 수 있는 곳들/places-can-go.cpp
+++ 250610/갈 수 있는 곳들/places-can-go.cpp
@@ -1,43 +1,32 @@
 #include <iostream>
-#include <queue>
+
 using namespace std;
 
 int n, k;
 int grid[100][100];
 int r[10000], c[10000];
-bool seen[100][100];
 
+int counter;
 int dx[] = {1,-1,0,0};
 int dy[] = {0,0,1,-1};
-int counter;
+bool seen[100][100] = {false};
 
-void bfs(int x, int y){
-    queue<pair<int,int>> q;
-    q.push({x,y});
+void dfs(int x,int y){
     if(seen[x][y] == true) return;
     seen[x][y] = true;
     counter++;
 
+    for(int i=0; i<4; i++){
+        int nx = x + dx[i];
+        int ny = y + dy[i];
 
-    while(!q.empty()){
-        pair<int,int> now = q.front();
-        q.pop();
-        int now_x = now.first;
-        int now_y = now.second;
-
-        for(int i=0; i<4; i++){
-            int nx = now_x + dx[i];
-            int ny = now_y + dy[i];
-
-            if(nx<1 || ny<1 || nx>n || ny>n) continue;
-            if(grid[nx][ny] == 0 && seen[nx][ny] == false){
-                q.push({nx,ny});
-                seen[nx][ny] = true;
-                counter++;
-            }
+        if(nx<1 || ny<1 || nx>n || ny>n) continue;
+        if(grid[nx][ny]==0 && seen[nx][ny] == false){
+            dfs(nx,ny);
         }
     }
 }
+
 
 
 int main() {
@@ -49,10 +38,9 @@ int main() {
         }
     }
 
-    
     for (int i = 0; i < k; i++) {
         cin >> r[i] >> c[i];
-        bfs(r[i],c[i]);
+        dfs(r[i],c[i]);
     }
 
     cout << counter;
