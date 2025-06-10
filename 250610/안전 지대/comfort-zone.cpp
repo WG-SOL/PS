1c61e9583fbf03e73fac23c4a8f0e5001322ea5a
WG-SOL <32182825@dankook.ac.kr>
2025-06-10 21:58:45 +0900

    Title: 안전 지대, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250610/안전 지대/comfort-zone.cpp
+++ 250610/안전 지대/comfort-zone.cpp
@@ -4,68 +4,57 @@ using namespace std;
 
 int N, M;
 int grid[50][50];
-int temp_map[50][50];
-int max_value; // 최댓값
-bool seen[50][50]={false}; //방문배열
-int area;
-int ans;
+bool seen[50][50];
+int dx[] = {1, -1, 0, 0};
+int dy[] = {0, 0, -1, 1};
 
-int dx[] = {1,-1,0,0};
-int dy[] = {0,0,-1,1};
+int max_value = 0;
+int ans = 0;
 
-
-void dfs(int x, int y){
+void dfs(int x, int y, int rain) {
     seen[x][y] = true;
 
-    for(int i=0; i<4; i++){
+    for (int i = 0; i < 4; i++) {
         int nx = x + dx[i];
         int ny = y + dy[i];
 
-        if(nx<0 || ny<0 || nx>=N || ny>=M) continue;
-        if(seen[nx][ny] == false && temp_map[nx][ny] > 0 ){
-            dfs(nx,ny);
+        if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
+        if (!seen[nx][ny] && grid[nx][ny] > rain) {
+            dfs(nx, ny, rain);
         }
     }
-
 }
-
-
 
 int main() {
     cin >> N >> M;
 
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < M; j++){
             cin >> grid[i][j];
         }
-    }
-    
-    for(int rain=1; rain<=100; rain++){
-        area=0;
-        for(int i=0; i<N; i++){
-            for(int j=0; j<M; j++){
-                temp_map[i][j] = grid[i][j] - rain;
-                seen[i][j] = false;
-            }
-        }
 
-        for(int i=0; i<N; i++){
-            for(int j=0; j<M; j++){
-                if(temp_map[i][j] > 0 && seen[i][j] == false){
-                    dfs(i,j);
+    for (int rain = 1; rain <= 100; rain++) {
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < M; j++)
+                seen[i][j] = false;
+
+        int area = 0;
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (grid[i][j] > rain && !seen[i][j]) {
+                    dfs(i, j, rain);
                     area++;
                 }
             }
         }
-        if(area > max_value){
+
+        if (area > max_value) {
+            max_value = area;
             ans = rain;
-            max_value = max(area,max_value);
         }
     }
 
-
     cout << ans << " " << max_value;
-    // Please write your code here.
-
     return 0;
 }
