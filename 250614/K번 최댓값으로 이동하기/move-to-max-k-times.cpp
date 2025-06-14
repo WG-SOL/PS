0c40869a3579ec607e7f80002fde313be4212626
WG-SOL <32182825@dankook.ac.kr>
2025-06-14 21:07:16 +0900

    Title: K번 최댓값으로 이동하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250614/K번 최댓값으로 이동하기/move-to-max-k-times.cpp
+++ 250614/K번 최댓값으로 이동하기/move-to-max-k-times.cpp
@@ -7,77 +7,75 @@ using namespace std;
 #define NOT_EXISTS make_pair(-1, -1)
 
 int n, k;
-int grid[101][101];
-bool visited[101][101];
+int grid[100][100];
+bool visited[100][100];
+pair<int, int> curr_cell;
 
 int dx[] = {0, 1, 0, -1};
 int dy[] = {1, 0, -1, 0};
 
 
-pair<int, int> bfs(int x, int y) {
+bool bfs() {
     queue<pair<int, int> > q;
     pair<int, int> best = NOT_EXISTS;
 
-    
-    for (int i = 1; i <= n; i++){
-        for (int j = 1; j <= n; j++){
-            visited[i][j] = false;
-        }
-    }
+    int sx = curr_cell.first;
+    int sy = curr_cell.second;
+    int curr_val = grid[sx][sy];
 
-    visited[x][y] = true;
-    q.push(make_pair(x, y));
-    int curr_val = grid[x][y];
+    // visited 초기화
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            visited[i][j] = false;
+
+    visited[sx][sy] = true;
+    q.push(make_pair(sx, sy));
 
     while (!q.empty()) {
-        int cx = q.front().first;
-        int cy = q.front().second;
-        q.pop();
+        pair<int, int> now = q.front(); q.pop();
+        int x = now.first;
+        int y = now.second;
 
         for (int i = 0; i < 4; i++) {
-            int nx = cx + dx[i];
-            int ny = cy + dy[i];
+            int nx = x + dx[i];
+            int ny = y + dy[i];
 
+            
             if (nx < 0 || ny < 0 || nx >= n || ny >= n) continue;
             if (visited[nx][ny] || grid[nx][ny] >= curr_val) continue;
 
             visited[nx][ny] = true;
             q.push(make_pair(nx, ny));
 
-            // best 업데이트
+            pair<int, int> next = make_pair(nx, ny);
             if (best == NOT_EXISTS ||
-                make_tuple(grid[nx][ny], -nx, -ny) >
-                make_tuple(grid[best.first][best.second], -best.first, -best.second)) {
-                best = make_pair(nx, ny);
-            }
+                make_tuple(grid[nx][ny], -nx, -ny) > make_tuple(grid[best.first][best.second], -best.first, -best.second))
+                best = next;
         }
     }
 
-    if (best == NOT_EXISTS) return make_pair(x, y); //(-1,-1)
+    if (best == NOT_EXISTS)
+        return false;
 
-    return best;
+    curr_cell = best;
+    return true;
 }
 
 int main() {
     cin >> n >> k;
-
-    for (int i = 1; i <= n; i++){
-        for (int j = 1; j <= n; j++){
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
             cin >> grid[i][j];
-        }
-    }
 
     int r, c;
     cin >> r >> c;
+    curr_cell = make_pair(r - 1, c - 1);
 
-    for (int i = 0; i < k; i++) {
-        pair<int, int> next = bfs(r, c);
-        if (next.first == r && next.second == c)
-            break; 
-        r = next.first;
-        c = next.second;
+    while (k--) {
+        if (!bfs())
+            break;
     }
 
-    cout << r << " " << c;
+    cout << curr_cell.first + 1 << " " << curr_cell.second + 1 << '\n';
     return 0;
 }
