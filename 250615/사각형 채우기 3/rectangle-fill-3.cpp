a6536829157a051f85d89ebe0d612b7466d7621f
WG-SOL <32182825@dankook.ac.kr>
2025-06-15 21:25:12 +0900

    Title: 사각형 채우기 3, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250615/사각형 채우기 3/rectangle-fill-3.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+long long DP[1001];
+
+int n;
+
+int main() {
+    cin >> n;
+    // 2 BY N을 채우는데
+    // 1 by 2 or 2 by 1에 1 by 1 사각형이 추가됨.
+    DP[0] = 1;
+    DP[1] = 2;
+    DP[2] = 7;
+
+    for (int i = 3; i <= n; i++) {
+        DP[i] = DP[i - 1] * 2 + DP[i - 2] * 3;
+
+        for (int j = 3; j <= i; j++) {
+            DP[i] += DP[i - j] * 2;
+        }
+    }
+
+    cout << DP[n];
+
+
+
+    // Please write your code here.
+
+    return 0;
+}
