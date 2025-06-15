dd395ed9abce7f800494c17b593cf80b5dc32a9d
WG-SOL <32182825@dankook.ac.kr>
2025-06-15 20:31:10 +0900

    Title: 피보나치 수, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250615/피보나치 수/fibonacci-number.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+long long DP[46];
+
+int main() {
+    cin >> N;
+    DP[1]=1;
+    DP[2]=1;
+
+    for(int i=3; i<=N; i++){
+        DP[i] = DP[i-1]+DP[i-2];
+    }
+
+    cout << DP[N];
+    // Please write your code here.
+    return 0;
+}
