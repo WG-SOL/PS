09b591e26b56621838869f84988d65e9032533a6
WG-SOL <32182825@dankook.ac.kr>
2025-06-15 21:33:48 +0900

    Title: 사각형 채우기 3, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250615/사각형 채우기 3/rectangle-fill-3.cpp
+++ 250615/사각형 채우기 3/rectangle-fill-3.cpp
@@ -12,13 +12,12 @@ int main() {
     // 1 by 2 or 2 by 1에 1 by 1 사각형이 추가됨.
     DP[0] = 1;
     DP[1] = 2;
-    DP[2] = 7;
 
-    for (int i = 3; i <= n; ++i) {
-    DP[i] = (DP[i - 1] * 2 % MOD + DP[i - 2] * 3 % MOD) % MOD;
+    for (int i = 2; i <= n; i++) {
+        DP[i] = (DP[i-1] * 2 + DP[i-2] * 3) % MOD;
 
-    for (int j = 3; j <= i; ++j) {
-        DP[i] = (DP[i] + DP[i - j] * 2 % MOD) % MOD;
+        for (int j = 3; j <= i; j++) {
+            DP[i] = (DP[i] + DP[i - j] * 2) % MOD;
     }
 }
 
