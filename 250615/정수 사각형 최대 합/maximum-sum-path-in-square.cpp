d805dc70162109ed1b3cd72777637588f26d8cbb
WG-SOL <32182825@dankook.ac.kr>
2025-06-15 23:04:46 +0900

    Title: 정수 사각형 최대 합, Time: 10ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250615/정수 사각형 최대 합/maximum-sum-path-in-square.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int n;
+long long grid[101][101];
+long long DP[101][101];
+
+int main() {
+    cin >> n;
+   
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            cin >> grid[i][j];
+        }
+    }
+
+    DP[1][1] = grid[1][1];
+
+    for(int i=2; i<=n; i++){
+        DP[1][i] = DP[1][i-1] + grid[1][i];
+        DP[i][1] = DP[i-1][1] + grid[i][1];
+    }
+
+
+    for (int i = 2; i <= n; i++) {
+        for (int j = 2; j <= n; j++) {
+            DP[i][j]=max(DP[i-1][j],DP[i][j-1]) + grid[i][j];
+        }
+    }
+
+    cout << DP[n][n];
+
+    // Please write your code here.
+
+    return 0;
+}
