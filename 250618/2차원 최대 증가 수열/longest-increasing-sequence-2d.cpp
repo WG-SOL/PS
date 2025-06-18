dc424875f50a1dfc715aab0a63bbbaa113165d90
WG-SOL <32182825@dankook.ac.kr>
2025-06-18 17:17:42 +0900

    Title: 2차원 최대 증가 수열, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250618/2차원 최대 증가 수열/longest-increasing-sequence-2d.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n, m;
+int grid[51][51];
+int DP[51][51];
+
+int main() {
+    cin >> n >> m;
+
+    for (int i = 1; i <= n; i++) 
+        for (int j = 1; j <= m; j++) 
+            cin >> grid[i][j];
+
+    
+    DP[1][1] = 1;
+
+    // 전체 탐색
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            if (DP[i][j] == 0) continue;
+
+            for (int x = i + 1; x <= n; x++) {
+                for (int y = j + 1; y <= m; y++) {
+                    if (grid[x][y] > grid[i][j]) {
+                        DP[x][y] = max(DP[x][y], DP[i][j] + 1);
+                    }
+                }
+            }
+        }
+    }
+
+    
+    int ans = 0;
+    for (int i = 1; i <= n; i++){
+        for (int j = 1; j <= m; j++){
+            ans = max(ans, DP[i][j]);
+        }
+    }
+    cout << ans;
+    return 0;
+}
