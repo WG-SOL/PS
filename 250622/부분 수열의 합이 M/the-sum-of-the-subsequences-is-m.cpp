54f55ccb4d927d9e5c8f9a71ab1b8ec27497b0c8
WG-SOL <32182825@dankook.ac.kr>
2025-06-22 18:54:41 +0900

    Title: 부분 수열의 합이 M, Time: 16ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 250622/부분 수열의 합이 M/the-sum-of-the-subsequences-is-m.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+int A[100];
+int DP[101];
+
+int main() {
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        cin >> A[i];
+    }
+
+    //DP INIT
+    DP[0]=0;
+    for(int i=1; i<=m; i++){
+        DP[i] = 101;
+    }
+
+    for(int i=0; i<n; i++){
+        for(int j=m; j>=0; j--){
+            if(j >= A[i]){
+                DP[j] = min(DP[j], DP[j-A[i]]+1);
+            }
+        }
+    }
+
+    int min_len = DP[m];
+
+    if(min_len == 101){
+        cout << -1;
+        return 0;
+    }
+    else{
+        cout << min_len;
+    }
+
+    // Please write your code here.
+
+    return 0;
+}
